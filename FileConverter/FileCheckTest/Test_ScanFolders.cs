ad781b88c74cff7b317f1ce4e7626a033a094f1c
Maerd-z <74662715+Maerd-z@users.noreply.github.com>
2024-09-23 05:05:47 +0200

    Added a test for directory traversal

---
+++ FileConverter/FileCheckTest/Test_ScanFolders.cs
@@ -0,0 +1,23 @@
+﻿using FileChecker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCheckTest
+{
+    [TestClass]
+    public class Test_ScanFolders
+    {
+        static string _baseDir = AppDomain.CurrentDomain.BaseDirectory;
+        [TestMethod]
+        public void ScanFolders_NestedFolders_FilesFound()
+        {
+            string startFolder = Path.Combine(_baseDir, @"..\..\..\TestFolders");
+
+            List<string> scannedFiles = Scanner.ScanFolders(startFolder);
+            Assert.IsTrue(scannedFiles.Contains("testfile2.jpg's signature matches JPG"));
+        }
+    }
+}
